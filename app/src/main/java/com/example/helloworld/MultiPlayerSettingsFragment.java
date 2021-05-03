96d87b76f98d89c5a9e2279a8064a37e30d2cb19
Chirag Madaan <66782780+chirag-madaan@users.noreply.github.com>
2021-05-04 00:22:31 +0530

    add multi-player settings

--- app/src/main/java/com/example/helloworld/MultiPlayerSettingsFragment.java
+++ app/src/main/java/com/example/helloworld/MultiPlayerSettingsFragment.java
@@ -9,10 +9,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MultiPlayerSettingsFragment extends Fragment {
 
-    String settingsData = "some multi player settings data";
+    String settingsData;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,10 +23,24 @@ public class MultiPlayerSettingsFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_multi_player_settings, container, false);
 
+        EditText circlePlayerName = (EditText) v.findViewById(R.id.circlePlayerName);
+        EditText crossPlayerName = (EditText) v.findViewById(R.id.crossPlayerName);
+
         Button startButton = (Button) v.findViewById(R.id.startButtonMultiPlayer);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+
+                RadioGroup radioGroup2 = (RadioGroup) v.findViewById(R.id.radioGroup2);
+                RadioButton radioButton2;
+                int radioId2 = radioGroup2.getCheckedRadioButtonId();
+                radioButton2 = v.findViewById(radioId2);
+                String radio = radioButton2.getText().toString();
+
+
+                settingsData = "m." + circlePlayerName.getText().toString() + "." +
+                                crossPlayerName.getText().toString() + "." + radio;
+
                 Intent intent = new Intent(getContext(), gameplayActivity.class);
                 intent.putExtra("settings_data", settingsData);
                 startActivity(intent);
